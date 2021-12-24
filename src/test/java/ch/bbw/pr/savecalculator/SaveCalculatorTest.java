529a71d5076131199e4782d76d5e42c1497d023e
LorisMueller <lomue@gmx.ch>
2021-12-24 14:14:23 +0100

    Added every Tests to one class with before and after

---
+++ src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
@@ -0,0 +1,39 @@
+package ch.bbw.pr.savecalculator;
+
+import junit.framework.TestCase;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.*;
+
+public class SaveCalculatorTest extends TestCase {
+
+    private SaveCalculator testee;
+
+    @Before
+    public void setUp() {
+        testee = new SaveCalculator();
+    }
+
+    @Test
+    public void testSummeTwoPositivesIsOk() {
+        int value1 = 10;
+        int value2 = 20;
+        assertTrue(testee.summe(value1, value2) == 30);
+    }
+
+    @Test
+    public void testSubtraktionTwoPositivesIsOk() {
+        int value1 = 50;
+        int value2 = 20;
+        assertTrue(testee.subtraktion(value1, value2) == 30);
+    }
+
+    @Test
+    public void testDivisionTwoPositivesIsOk() {
+        int value1 = 100;
+        int value2 = 10;
+        assertTrue(testee.subtraktion(value1, value2) == 10);
+    }
+}
