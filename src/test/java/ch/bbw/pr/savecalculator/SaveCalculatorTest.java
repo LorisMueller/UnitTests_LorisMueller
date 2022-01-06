274c9b09964dfa446475e2b1d7c3c9721e14ed56
LorisMueller <lomue@gmx.ch>
2022-01-06 16:53:12 +0100

    added expected and unexpected Exception

--- src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
+++ src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
@@ -2,12 +2,13 @@ package ch.bbw.pr.savecalculator;
 
 import junit.framework.TestCase;
 import org.junit.*;
+import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
 
 import java.util.*;
 
-public class SaveCalculatorTest extends TestCase {
+public class SaveCalculatorTest {
 
     private SaveCalculator testee;
 
@@ -36,6 +37,22 @@ public class SaveCalculatorTest extends TestCase {
         int value2 = 10;
         assertTrue(testee.division(value1, value2) == 10);
     }
+
+    @Test(expected = java.lang.ArithmeticException.class)
+    public void testArithmeticException() {
+        int value1 = Integer.MAX_VALUE;
+        int value2 = 1;
+        testee.summe(value1, value2);
+    }
+
+    @Test
+    public void testArithmeticException_2()
+        throws ArithmeticException {
+        int value1 = Integer.MAX_VALUE;
+        int value2 = 1;
+        testee.summe(value1, value2);
+    }
+
 
     //Only one assertions each, is already added in the previous step
     
